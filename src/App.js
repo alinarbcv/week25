17825b338cab75160d42424339625aef80482bc4
alinarbcv <alinarubtsovaa@gmail.com>
2024-11-24 22:59:04 +0100

    week 25 zad 1

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import './App.css';
+import { Pie } from 'react-chartjs-2';
+import Chart from 'chart.js/auto';
+import Counter from './Counter';
+
+function App() {
+  const data = {
+    labels: ['Schlafen', 'Arbeit', 'Freizeit', 'Essen'],
+    datasets: [
+      {
+        label: 'part',
+        data: [8, 10, 3, 5],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(25, 66, 77, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+  return (
+    <div className="App"> 
+    <div style={{width:'400px',height:'400px'}}>
+    <Pie data={data} />
+    </div>
+    <h1>React Counter</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Counter.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+function Counter() {
+    const [count, setCount] = useState(0);
+    const handleClick = () => {
+        setCount(count + 1);
+      };
+    
+      return <button onClick={handleClick}>{count}</button>;
+    }
+    
+    export default Counter;
